27e7e8afdbe32e26e39fb8a3ba765e9de44b21ce
Kauh4L <133434010+Kauh4L@users.noreply.github.com>
2026-06-22 22:33:27 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// declarações das funções (ou inclui os .h se tiver)
+
+void bubbleSort(int vetor[], int n);
+void quickSort(int vetor[], int inicio, int fim);
+void dijkstra(int grafo[5][5], int origem);
+
+int main() {
+
+    printf("Testes dos algoritmos\n");
+
+    // aqui você chama os testes de cada um
+
+    return 0;
+}
+
